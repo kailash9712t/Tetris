849c698bb6f0d9c2ed2519503d7e7e099ff527fd
kailash <kailashdewasi620@gmail.com>
2025-04-05 17:51:16 +0530

    port issue

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 import app from './app.js';
 import http from 'http';
-import {Server} from 'socket.io';
-import SetUpServer from './src/WebSocket/index.js'
+import { Server } from 'socket.io';
+import SetUpServer from './src/WebSocket/index.js';
+
+// Use process.env.PORT or default to an available port (like 3000).
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors : {
-        origin : "*",
-        methods : ["GET","POST"]
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
     }
 });
- 
+
 SetUpServer(io);
 
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
     console.log("Web Socket Connected ... !");
     console.log("Express Server is running ... !");
-    console.log(process.env.PORT);
-})
+    console.log(`Server is running on port: ${PORT}`);
+});
